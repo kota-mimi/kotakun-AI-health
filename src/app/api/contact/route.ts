93bcd37ac05102da7c0850260ee42cda512aa192
kotakun-developer <kotakun@example.com>
2025-10-25 22:25:22 +0900

    Nodemailerのimport方法を修正

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 
 export async function POST(request: NextRequest) {
   try {
